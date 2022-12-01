96af2e3bab3c78bf2bc7e13f2b1ab0c12dade1e6
Naookie Sato <naookie@amazon.com>
2022-12-01 10:40:39 -0800

    added timing

--- 2022/day01/solution.py
+++ 2022/day01/solution.py
@@ -1,12 +1,16 @@
+import time 
+start = time.time()
 elves = {1: 0}
 i = 1
 with open('input2.txt', 'r') as f:
-    for line in f.readlines():
-        line = line.strip()
+    for line in [l.strip() for l in f.readlines()]:
         if line != '':
             elves[i] += int(line)
         else:
             i+=1
             elves[i] = 0
-print('part 1: ' + str(list(reversed(sorted(elves.items(), key=lambda item: item[1])))[0]))
-print('part 2: ' + str(sum([e[1] for e in list(reversed(sorted(elves.items(), key=lambda item: item[1])))[0:3]])))
+by_cals = list(reversed(sorted(elves.items(), key=lambda item: item[1])))
+print('part 1: ' + str(by_cals[0]))
+print('part 2: ' + str(sum([e[1] for e in by_cals[0:3]])))
+end = time.time()
+print('Took {} seconds'.format(end-start))
