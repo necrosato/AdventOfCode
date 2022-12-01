ea7f48b1da4abf558fa3624f4f803d2e28cf6f71
Naookie Sato <naookie@amazon.com>
2022-12-01 10:30:09 -0800

    consolidated

--- 2022/day01/solution.py
+++ 2022/day01/solution.py
@@ -8,4 +8,5 @@ with open('input2.txt', 'r') as f:
         else:
             i+=1
             elves[i] = 0
-print(list(reversed(sorted(elves.items(), key=lambda item: item[1])))[0])
+print('part 1: ' + str(list(reversed(sorted(elves.items(), key=lambda item: item[1])))[0]))
+print('part 2: ' + str(sum([e[1] for e in list(reversed(sorted(elves.items(), key=lambda item: item[1])))[0:3]])))
