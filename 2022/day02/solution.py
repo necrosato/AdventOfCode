024e363a1111f6e84b5e62b8f61badcd8f82d776
Naookie Sato <naookie@amazon.com>
2022-12-02 10:06:28 -0800

    less lines

--- 2022/day02/solution.py
+++ 2022/day02/solution.py
@@ -1,6 +1,5 @@
 import time
 shapes = { 'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3 }
-# returns points for p2
 def play( p1, p2 ):
     if p1 == p2:
         return p2 + 3
@@ -13,15 +12,12 @@ def play2( p1, outcome ):
     if outcome == 'Z':
         return (p1%3)+1 + 6
     return 3 if p1 == 1 else p1-1
-
 score = 0
 score2 = 0
 with open('input2.txt', 'r') as f:
     start = time.time()
-    for line in [l.strip() for l in f.readlines()]:
-        p1, p2 = line.split()
+    for p1, p2 in [l.strip().split() for l in f.readlines()]:
         score += play(shapes[p1], shapes[p2])
         score2 += play2(shapes[p1], p2)
     end = time.time()
-
 print('part 1: {} part 2: {} took {} seconds'.format(score, score2, end-start))
