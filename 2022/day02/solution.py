97135e68c8c300fd5306dd5aea49289fd301315c
Naookie Sato <naookie@amazon.com>
2022-12-02 08:42:04 -0800

    consolidated

--- 2022/day02/solution.py
+++ 2022/day02/solution.py
@@ -7,13 +7,21 @@ def play( p1, p2 ):
     if (p1%3)+1 == p2:
         return p2 + 6
     return p2
+def play2( p1, outcome ):
+    if outcome == 'Y':
+        return p1 + 3
+    if outcome == 'Z':
+        return (p1%3)+1 + 6
+    return 3 if p1 == 1 else p1-1
 
 score = 0
+score2 = 0
 with open('input2.txt', 'r') as f:
     start = time.time()
     for line in [l.strip() for l in f.readlines()]:
         p1, p2 = line.split()
         score += play(shapes[p1], shapes[p2])
+        score2 += play2(shapes[p1], p2)
     end = time.time()
 
-print('{} took {} seconds'.format(score, end-start))
+print('part 1: {} part 2: {} took {} seconds'.format(score, score2, end-start))
