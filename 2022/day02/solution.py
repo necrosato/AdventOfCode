b715931d3f06e7ae097dd3f813d96548a9a3576d
Naookie Sato <naookie@amazon.com>
2022-12-02 07:29:18 -0800

    timing

--- 2022/day02/solution.py
+++ 2022/day02/solution.py
@@ -1,23 +1,19 @@
+import time
 shapes = { 'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3 }
-
 # returns points for p2
 def play( p1, p2 ):
     if shapes[p1] == shapes[p2]:
-        print("Tie!")
         return shapes[p2] + 3
     if (shapes[p1]%3)+1 == shapes[p2]:
-        print("P2 Wins!")
         return shapes[p2] + 6
-    print("P1 Wins!")
     return shapes[p2]
-
 
 score = 0
 with open('input2.txt', 'r') as f:
+    start = time.time()
     for line in [l.strip() for l in f.readlines()]:
         p1, p2 = line.split()
-        print('{} vs {}'.format(shapes[p1], shapes[p2]))
         score += play(p1, p2)
-        print(score)
+    end = time.time()
 
-print(score)
+print('{} took {} seconds'.format(score, end-start))
