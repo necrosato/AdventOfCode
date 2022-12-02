d7d0ec6142bc4e3951845cf534160e9ba262d9b3
Naookie Sato <naookie@amazon.com>
2022-12-02 08:38:07 -0800

    better caching

--- 2022/day02/solution.py
+++ 2022/day02/solution.py
@@ -2,18 +2,18 @@ import time
 shapes = { 'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3 }
 # returns points for p2
 def play( p1, p2 ):
-    if shapes[p1] == shapes[p2]:
-        return shapes[p2] + 3
-    if (shapes[p1]%3)+1 == shapes[p2]:
-        return shapes[p2] + 6
-    return shapes[p2]
+    if p1 == p2:
+        return p2 + 3
+    if (p1%3)+1 == p2:
+        return p2 + 6
+    return p2
 
 score = 0
 with open('input2.txt', 'r') as f:
     start = time.time()
     for line in [l.strip() for l in f.readlines()]:
         p1, p2 = line.split()
-        score += play(p1, p2)
+        score += play(shapes[p1], shapes[p2])
     end = time.time()
 
 print('{} took {} seconds'.format(score, end-start))
