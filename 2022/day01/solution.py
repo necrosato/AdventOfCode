5774f906a1a4f6f98244332714773f3417fddc01
Naookie Sato <naookie@amazon.com>
2022-12-02 06:36:47 -0800

    better timing

--- 2022/day01/solution.py
+++ 2022/day01/solution.py
@@ -9,8 +9,11 @@ with open('input2.txt', 'r') as f:
         else:
             i+=1
             elves[i] = 0
+parsed = time.time()
+print('Mapping calories took {} milliseconds'.format((parsed-start)*1000))
 by_cals = list(reversed(sorted(elves.items(), key=lambda item: item[1])))
 print('part 1: ' + str(by_cals[0]))
 print('part 2: ' + str(sum([e[1] for e in by_cals[0:3]])))
 end = time.time()
-print('Took {} seconds'.format(end-start))
+print('Calculating solutions took {} milliseconds'.format((end-parsed)*1000))
+print('Total took {} milliseconds'.format((end-start)*1000))
