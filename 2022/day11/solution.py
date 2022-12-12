67011b903deee1f804caa0b1e4c6ab587e3828fc
Naookie Sato <naookie@amazon.com>
2022-12-12 08:58:18 -0800

    fast

--- 2022/day11/solution.py
+++ 2022/day11/solution.py
@@ -1,5 +1,9 @@
 import time
 
+class Item:
+    def __init__(self, val):
+        self.val = val
+
 class Monkey:
     def __init__(self, n, items, operation, test, if_true, if_false):
         self.n = n
@@ -9,52 +13,61 @@ class Monkey:
         self.if_true = if_true
         self.if_false = if_false
         self.throws = 0
+        self.maxWorry = 0
     
-    def throw(self, i, to, monkeys):
-        #print('monkey {} throwing {} to monkey {}'.format(self.n, i, to))
-        monkeys[to].items.append(i)
+    def throw(self, item, to, monkeys):
+        monkeys[to].items.append(item)
 
     def turn(self, monkeys):
         for i in range(len(self.items)):
-            o = self.operate(self.items[i]) // 3
-            if o % self.test == 0:
+            o = self.operate(self.items[i])
+            if self.testItem(o):
                 self.throw(o, self.if_true, monkeys)
             else:
                 self.throw(o, self.if_false, monkeys)
             self.throws += 1
         self.items = []
 
-    def operate(self, n):
-        f = n if self.operation[1] == 'old' else int(self.operation[1])
-        if self.operation[0] == '+':
-            return n + f
-        if self.operation[0] == '*':
-            return n * f
+    def testItem(self, item):
+        item.val = item.val % self.maxWorry
+        return item.val % self.test == 0
 
+    def operate(self, item):
+        f = item.val if self.operation[1] == 'old' else int(self.operation[1])
+        operation = (self.operation[0], f)
+        if self.operation[0] == '+':
+            item.val += f
+        if self.operation[0] == '*':
+            item.val *= f
+# set to true for part 1, false for part2
+        if False:
+            item.val //= 3
+        return item
 
 for fname in ['input.txt', 'input2.txt']:
     solution = 0
-    solution2 = 0
     with open(fname, 'r') as f:
         monkeys = []
+        maxWorry = 1
         for chunk in f.read().split('\n\n'):
             chunk = chunk.split('\n')
-            n = chunk[0].split()[1][0]
-            items = [int(i) for i in chunk[1].split(':')[1].strip().split(', ')]
-            operation = chunk[2].split()[-2:]
+            n = int(chunk[0].split()[1][0])
+            items = [Item(int(i)) for i in chunk[1].split(':')[1].strip().split(', ')]
+            operation = tuple(chunk[2].split()[-2:])
             test = int(chunk[3].split()[-1])
+            maxWorry *= test
             if_true = int(chunk[4].split()[-1])
             if_false = int(chunk[5].split()[-1])
-            #print((items, operation, test, if_true, if_false))
             monkeys.append(Monkey(n, items, operation, test, if_true, if_false))
+        for monkey in monkeys:
+            monkey.maxWorry = maxWorry
+
         start = time.time()
-        rounds = 300
+        rounds = 10000
         for i in range(rounds):
             for monkey in monkeys:
                 monkey.turn(monkeys)
-
-        #print([m.throws for m in monkeys])
         solution = list(sorted(monkeys, key=lambda x: x.throws))[-2:]
         solution = solution[0].throws * solution[1].throws
         end = time.time()
-        print('{} solution: {} solution2: {} took {} seconds'.format(fname, solution, solution2, end-start))
+        print('{} solution: {} took {} seconds'.format(fname, solution, end-start))
