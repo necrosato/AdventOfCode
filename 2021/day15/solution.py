983d7e6556cf81d209841ace44db0d765c5b3875
Naookie Sato <naookie@amazon.com>
2022-12-11 18:14:46 -0800

    faster

--- 2021/day15/solution.py
+++ 2021/day15/solution.py
@@ -9,37 +9,28 @@ def getAdjacent(i, j, maxi, maxj):
             adjacent.append((x, y))
     return adjacent
 
-def dijkstra( grid, i, j, endi, endj, distances, unvisited ):
+def dijkstra( grid, heap, end, visited ):
+    dist, i, j = heapq.heappop(heap)
     for x, y in getAdjacent(i, j, len(grid)-1, len(grid[0])-1):
-        if (x, y) in unvisited:
-            dist = grid[x][y] + distances[(i,j)]
-            if (x, y) not in distances or distances[(x, y)] > dist:
-                distances[(x, y)] = dist
-    unvisited.remove((i, j))
-    if i != endi or j != endj:
-        for pair in list(sorted(distances.items(), key=lambda p: p[1])):
-            if pair[0] in unvisited:
-                return pair[0]
-    return None
+        if (x, y) not in visited:
+            nd = grid[x][y] + dist
+            visited.add((x, y))
+            heapq.heappush(heap, (nd, x, y))
+    if i != end[0] or j != end[1]:
+        return heap
+    return dist + grid[-1][-1] - grid[0][0]
 
 for fname in ['input.txt', 'input2.txt', 'input3.txt']:
-    solution = 0
     grid = []
-    distances = {}
     with open(fname, 'r') as f:
         lines = [l.strip() for l in f.readlines()]
         start = time.time()
         for line in lines:
             grid.append([int(c) for c in line])
-        distances[(len(grid)-1, len(grid[0])-1)] = 0
-        unvisited = set()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                unvisited.add((i, j))
-        node = (len(grid)-1, len(grid[0])-1)
-        while node is not None:
-            node = dijkstra(grid, node[0], node[1], 0, 0, distances, unvisited)
+        visited = set()
+        heap = [(0, len(grid)-1, len(grid[0])-1)]
+        while type(heap) is not int:
+            heap = dijkstra(grid, heap, (0, 0), visited)
 
-        solution = distances[(0,0)] + grid[-1][-1] - grid[0][0]
         end = time.time()
-        print('{} solution: {} took {} seconds'.format(fname, solution, end-start))
+        print('{} solution: {} took {} seconds'.format(fname, heap, end-start))
