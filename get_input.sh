15b81396221f8823bbd5ea634414c1f8030e0c5b
Naookie Sato <naookie@amazon.com>
2022-12-12 16:12:30 -0800

    input download script

---
+++ get_input.sh
@@ -0,0 +1,1 @@
+curl --cookie "session=$1" https://adventofcode.com/$2/day/$3/input
---
+++ new_day.sh
@@ -0,0 +1,6 @@
+set -x
+dir=$(dirname ${0:a})
+ndp=$dir/$1/day$2
+mkdir -p $ndp
+cp $dir/template/* $ndp
+curl --cookie "session=$(cat $dir/cookie)" https://adventofcode.com/$1/day/$2/input | tee $ndp/input.txt
