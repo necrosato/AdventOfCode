f91cda6ea0dd421676eb35dc59b6d6d5a50200ba
Naookie Sato <naookie@amazon.com>
2022-12-14 13:10:17 -0800

    cleanup

--- 2022/day14/solution.py
+++ 2022/day14/solution.py
@@ -5,7 +5,6 @@ class Sand:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
     def fall(self, world):
         if not world.hasSolid((self.x, self.y+1)):
             self.y += 1
@@ -19,14 +18,12 @@ class Sand:
             self.y += 1
             return True
         return False
-
     def fallUntilForever(self, world):
         while not ( self.x not in world.maxys or self.y >= world.maxys[self.x] ):
             if not self.fall(world):
                 world.sands.add((self.x, self.y))
                 return False
         return True
-
     def fallUntilFloor(self, world):
         while self.fall(world):
             pass
@@ -47,34 +44,6 @@ class World:
         if self.floor > 0 and self.floor - coords[1] == 0:
             return True
         return False
-    def __repr__(self):
-        s = ''
-        minx = 500
-        maxx = 500
-        miny = 0
-        maxy = 0
-        for rock in self.rocks.union(self.sands):
-            maxx = max(maxx, rock[0])
-            maxy = max(maxy, rock[1])
-            minx = min(minx, rock[0])
-            miny = min(miny, rock[1])
-        for i in range(maxy-miny+1):
-            for j in range(maxx-minx+1):
-                if (minx+j, miny+i) in self.rocks:
-                    s += '#'
-                elif (minx+j,miny+i) in self.sands:
-                    s += 'o'
-                else:
-                    s += '.'
-            s += '\n'
-        return s
-
-def lineCoords( c1, c2 ):
-    lc = set()
-    for x in range(abs(c1[0] - c2[0])+1):
-        for y in range(abs(c1[1] - c2[1])+1):
-            lc.add((x+min(c1[0], c2[0]), y+min(c1[1], c2[1])))
-    return lc
 
 def part1(world, source):
     sol = 0
@@ -103,11 +72,9 @@ for fname in ['input', 'input2'] if len(sys.argv) < 2 else sys.argv[1:]:
         for line in lines:
             points = line.split(' -> ')
             for i in range(len(points)-1):
-                x, y = points[i].split(',')
-                p1 = (int(x), int(y))
-                x, y = points[i+1].split(',')
-                p2 = (int(x), int(y))
-                for rock in lineCoords(p1, p2):
+                p1 = [int(n)for n in points[i].split(',')]
+                p2 = [int(n)for n in points[i+1].split(',')]
+                for rock in [(x+min(p1[0], p2[0]), y+min(p1[1], p2[1])) for x in range(abs(p1[0] - p2[0])+1) for y in range(abs(p1[1] - p2[1])+1)]:
                     rocks.add(rock)
                     maxy = max(rock[1], maxy)
                     if rock[0] not in maxys:
