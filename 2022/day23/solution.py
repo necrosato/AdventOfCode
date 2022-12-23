ee3ea8d8c77c319a44feca638482ac9c8626e4fb
Naookie Sato <naookie@amazon.com>
2022-12-23 10:11:26 -0800

    compressed a bit

--- 2022/day23/solution.py
+++ 2022/day23/solution.py
@@ -1,7 +1,9 @@
 import time
 import sys
 
+directions = 'NSWE'
 offsets = {'N': ([-1, 0, 1], [-1]), 'S': ([-1, 0, 1], [1]), 'W': ([-1], [-1, 0, 1]), 'E': ([1], [-1, 0, 1])}
+
 def getFreeNeighbors(source, offset, elves):
     neighbors = [(source[0] + xo, source[1] + yo) for xo in offset[0] for yo in offset[1]]
     for neighbor in neighbors:
@@ -18,22 +20,8 @@ def shouldMove(x, y, elves):
     return False
 
 def getDimensions(elves):
-    xmin = min([elf[0] for elf in elves])
-    ymin = min([elf[1] for elf in elves])
-    xmax = max([elf[0] for elf in elves])
-    ymax = max([elf[1] for elf in elves])
-    return (1 + xmax - xmin, 1 + ymax - ymin)
+    return (1 + max([elf[0] for elf in elves]) - min([elf[0] for elf in elves]), 1 + max([elf[1] for elf in elves]) - min([elf[1] for elf in elves]))
 
-def printElves(elves):
-    tx, ty = getDimensions(elves)
-    xmin = min([elf[0] for elf in elves])
-    ymin = min([elf[1] for elf in elves])
-    grid = [['.' for j in range(tx)] for i in range(ty)]
-    for elf in elves:
-        grid[elf[1]-ymin][elf[0]-xmin] = '#'
-    for g in grid:
-        print(''.join(g))
- 
 def executeRound(elves, directions):
     propositions = {}
     movements = 0
@@ -54,24 +42,16 @@ def executeRound(elves, directions):
     return movements
 
 def part1( elves ):
-    directions = 'NSWE'
     i = 0
-    while i < 10:
-        i += 1
-        executeRound(elves, directions)
-        directions = directions[1:] + directions[0]
-    tx, ty = getDimensions(elves)
-    return tx*ty - len(elves)
+    while i < 10 and executeRound(elves, directions[i%4:4]+directions[0:i%4]) > 0:
+        i+=1
+    return getDimensions(elves)[0]*getDimensions(elves)[1] - len(elves)
 
 def part2( elves ):
-    directions = 'NSWE'
-    movements = 1
     i = 0
-    while movements > 0:
-        i += 1
-        movements = executeRound(elves, directions)
-        directions = directions[1:] + directions[0]
-    return i
+    while executeRound(elves, directions[i%4:4]+directions[0:i%4]) > 0:
+        i+=1
+    return i+1
 
 for fname in ['input', 'input2'] if len(sys.argv) < 2 else sys.argv[1:]:
     with open(fname, 'r') as f:
