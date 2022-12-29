591481eb9e7817ec476a721583bd70622143ffad
Naookie Sato <naookie@amazon.com>
2022-12-29 11:39:51 -0800

    2022 done!

--- 2022/day19/solution.py
+++ 2022/day19/solution.py
@@ -5,29 +5,20 @@ import sys
 def part1( simulations ):
     qualities = []
     for simulation in simulations:
-        print('starting ' + simulation)
         s = time.time()
         vals, sim = simulations[simulation].run(24, set())
-        e = time.time()
-        print('{} took {} seconds'.format(simulation, e-s))
-        simulations[simulation] = sim
-        quality = vals[1][0] * simulations[simulation].blueprint.id
-        qualities.append(quality)
-        print('simulation: {} vals: {} quality: {}'.format(simulation, vals, quality))
+        qualities.append(vals[1][0] * sim.blueprint.id)
+        print('simulation: {} vals: {} quality: {} took {} seconds'.format(simulation, vals, qualities[-1], time.time()-s))
     return sum(qualities)
         
 def part2( simulations ):
     geodes = []
     for simulation in ['Blueprint 1', 'Blueprint 2', 'Blueprint 3']:
-        print('starting ' + simulation)
-        s = time.time()
-        vals, sim = simulations[simulation].run(32, set())
-        e = time.time()
-        print('{} took {} seconds'.format(simulation, e-s))
-        simulations[simulation] = sim
-        gs = vals[1][0]
-        geodes.append(gs)
-        print('simulation: {} vals: {} geodes: {}'.format(simulation, vals, gs))
+        if simulation in simulations:
+            s = time.time()
+            vals, sim = simulations[simulation].run(32, set())
+            geodes.append(vals[1][0])
+            print('simulation: {} vals: {} geodes: {} took {} seconds'.format(simulation, vals, geodes[-1], time.time()-s))
     prod = 1
     for gs in geodes:
         prod *= gs
@@ -67,37 +58,50 @@ class Simulation:
     def optimalGeodes(self, minutes):
         return self.geo + sum([(self.geo_bots + i) for i in range(minutes)])
 
-
-    def run(self, minutes, visited):
+    def run(self, minutes, visited, currentMax=None):
         if self.state() in visited or minutes == 0:
             visited.add(self.state())
             return self.state(), self
+        if currentMax is None:
+            currentMax = (self.state(), self)
+        if currentMax[0][1][0] >= self.optimalGeodes(minutes):
+            return currentMax
         visited.add(self.state())
-        options = [self]
+        options = []
         if self.ore >= self.blueprint.geo_bot_cost[0] and self.obs >= self.blueprint.geo_bot_cost[1]:
+            if self.ore_bots >= self.blueprint.geo_bot_cost[0] and self.obs_bots >= self.blueprint.geo_bot_cost[1]:
+                self.geo = self.optimalGeodes(minutes)
+                return self.state(), self
             options.append(Simulation(other=self))
             options[-1].ore -= self.blueprint.geo_bot_cost[0]
             options[-1].obs -= self.blueprint.geo_bot_cost[1]
             options[-1].collect()
             options[-1].geo_bots += 1
-        if self.ore >= self.blueprint.obs_bot_cost[0] and self.cla >= self.blueprint.obs_bot_cost[1] and self.obs_bots < self.blueprint.geo_bot_cost[1]:
-            options.append(Simulation(other=self))
-            options[-1].ore -= self.blueprint.obs_bot_cost[0]
-            options[-1].cla -= self.blueprint.obs_bot_cost[1]
-            options[-1].collect()
-            options[-1].obs_bots += 1
-        if self.ore >= self.blueprint.cla_bot_cost and self.cla_bots < self.blueprint.obs_bot_cost[1]:
-            options.append(Simulation(other=self))
-            options[-1].ore -= self.blueprint.cla_bot_cost
-            options[-1].collect()
-            options[-1].cla_bots += 1
-        if self.ore >= self.blueprint.ore_bot_cost and self.ore_bots < max([self.blueprint.geo_bot_cost[0], self.blueprint.obs_bot_cost[0], self.blueprint.cla_bot_cost, self.blueprint.ore_bot_cost]):
-            options.append(Simulation(other=self))
-            options[-1].ore -= self.blueprint.ore_bot_cost
-            options[-1].collect()
-            options[-1].ore_bots += 1
+        else:
+            options.append(self)
+            if self.ore >= self.blueprint.obs_bot_cost[0] and self.cla >= self.blueprint.obs_bot_cost[1] and self.obs_bots < self.blueprint.geo_bot_cost[1]:
+                options.append(Simulation(other=self))
+                options[-1].ore -= self.blueprint.obs_bot_cost[0]
+                options[-1].cla -= self.blueprint.obs_bot_cost[1]
+                options[-1].collect()
+                options[-1].obs_bots += 1
+            if self.ore >= self.blueprint.cla_bot_cost and self.cla_bots < self.blueprint.obs_bot_cost[1]:
+                options.append(Simulation(other=self))
+                options[-1].ore -= self.blueprint.cla_bot_cost
+                options[-1].collect()
+                options[-1].cla_bots += 1
+            if self.ore >= self.blueprint.ore_bot_cost and self.ore_bots < max([self.blueprint.geo_bot_cost[0], self.blueprint.obs_bot_cost[0], self.blueprint.cla_bot_cost, self.blueprint.ore_bot_cost]):
+                options.append(Simulation(other=self))
+                options[-1].ore -= self.blueprint.ore_bot_cost
+                options[-1].collect()
+                options[-1].ore_bots += 1
         self.collect()
-        return max([option.run(minutes-1, visited) for option in options], key=lambda x:x[0])
+        results = []
+        for option in options:
+            if len(results) > 0 and currentMax[0][1][0] < results[-1][0][1][0]:
+                currentMax = results[-1]
+            results.append(option.run(minutes-1, visited, currentMax))
+        return max(results, key=lambda x:x[0])
 
     def collect(self):
         self.ore += self.ore_bots
@@ -113,19 +117,18 @@ for fname in ['input', 'input2'] if len(sys.argv) < 2 else sys.argv[1:]:
     with open(fname, 'r') as f:
         ts = time.time()
         lines = [l.strip() for l in f.readlines()]
-        blueprints = {}
         simulations = {}
+        simulations2 = {}
         for line in lines:
             name, instructions = line.split(': ')
-            instructions = instructions.split('. ')
-            blueprints[name] = Blueprint(name, instructions)
-            simulations[name] = Simulation(blueprints[name])
+            blueprint = Blueprint(name, instructions.split('. '))
+            simulations[name] = Simulation(blueprint)
+            simulations2[name] = Simulation(blueprint)
         p1 = time.time()
-        #sol1 = part1(simulations)
-        sol1 = 0
+        sol1 = part1(simulations)
         p2 = time.time()
-        sol2 = part2(simulations)
-        te = time.time()
         print('{} part1: {} took {} seconds'.format(fname, sol1, p2-p1))
+        sol2 = part2(simulations2)
+        te = time.time()
         print('{} part2: {} took {} seconds'.format(fname, sol2, te-p2))
         print('{} total time {} seconds'.format(fname, te-ts))
