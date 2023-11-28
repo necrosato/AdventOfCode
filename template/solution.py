e02ef1dd647ed35b69801cb677ace77172ee46c5
Naookie Sato <naookie@amazon.com>
2023-11-28 12:43:06 -0800

    updated template

--- template/solution.py
+++ template/solution.py
@@ -1,21 +1,38 @@
+import argparse
 import time
-import sys
 
+def timer_func(func):
+    def wrap_func(*args, **kwargs):
+        t1 = time.time()
+        result = func(*args, **kwargs)
+        t2 = time.time()
+        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s returned {result}')
+        return result
+    return wrap_func
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Advent of Code Solution', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input', type=str, action='append', required=True,
+        help='an input file path, can passed multiple times to run multiple test files')
+    return parser.parse_args()
+
+@timer_func
 def part1( grid ):
     pass
+
+@timer_func
 def part2( grid ):
     pass
 
-for fname in ['input', 'input2'] if len(sys.argv) < 2 else sys.argv[1:]:
-    with open(fname, 'r') as f:
-        ts = time.time()
-        lines = [l.strip() for l in f.readlines()]
-        grid = []
-        p1 = time.time()
-        sol1 = part1(grid)
-        p2 = time.time()
-        sol2 = part2(grid)
-        te = time.time()
-        print('{} part1: {} took {} seconds'.format(fname, sol1, p2-p1))
-        print('{} part2: {} took {} seconds'.format(fname, sol2, te-p2))
-        print('{} total time {} seconds'.format(fname, te-ts))
+def main():
+    args = parseArgs()
+    for input_file_name in args.input:
+        with open(input_file_name, 'r') as f:
+            lines = [l.strip() for l in f.readlines()]
+            grid = []
+            sol1 = part1(grid)
+            sol2 = part2(grid)
+        
+if __name__=='__main__':
+    main()
+
