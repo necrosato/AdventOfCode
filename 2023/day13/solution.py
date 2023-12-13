4b526bf96966fb24779cd84558d3db4051a1cd3e
Naookie Sato <naookie@amazon.com>
2023-12-13 11:07:22 -0800

    cleanup

--- 2023/day13/solution.py
+++ 2023/day13/solution.py
@@ -16,57 +16,36 @@ def parseArgs():
         help='an input file path, can passed multiple times to run multiple test files')
     return parser.parse_args()
 
-def check_fold(block, i):
-    check_len = min(i, len(block)-i)
-    return list(reversed(block[i-check_len:i])) == block[i:i+check_len]
-
 def string_mismatch(s1, s2):
     return sum([ s1[i] != s2[i] for i in range(len(s1))])
          
-def check_smudge(block, i):
+def check_fold(block, i, tolerance):
     check_len = min(i, len(block)-i)
     l = list(reversed(block[i-check_len:i]))
     r = block[i:i+check_len]
-    return sum([string_mismatch(l[i], r[i]) for i in range(check_len)]) == 1
+    return sum([string_mismatch(l[i], r[i]) for i in range(check_len)]) == tolerance
 
 def rotate(block):
     return [ ''.join([ block[r][c] for r in range(len(block)) ]) for c in range(len(block[0]))]
 
-def part(blocks, check_f):
+@timer_func
+def part(blocks, tolerance):
     total = 0
-    for hb, vb in blocks:
-        for i in range(1, len(hb)):
-            if check_f(hb, i):
-                total += i * 100
-                break
-        for i in range(1, len(vb)):
-            if check_f(vb, i):
-                total += i
-                break
+    for pairs in blocks:
+        for pair in pairs:
+            for i in range(1, len(pair[0])):
+                if check_fold(pair[0], i, tolerance):
+                    total += i * pair[1]
     return total
-
-@timer_func
-def part1( blocks ):
-    return part(blocks, check_fold)
-
-@timer_func
-def part2( blocks ):
-    return part(blocks, check_smudge)
 
 def main():
     args = parseArgs()
     for input_file_name in args.input:
         with open(input_file_name, 'r') as f:
-            lines = [l.strip() for l in f.readlines()]
-            blocks = []
-            bi = 0
-            for i in range(len(lines)):
-                if lines[i] == '':
-                    blocks.append((lines[bi:i], rotate(lines[bi:i])))
-                    bi = i+1
-            blocks.append((lines[bi:], rotate(lines[bi:])))
-            sol1 = part1(blocks)
-            sol2 = part2(blocks)
+            blocks = f.read().strip().split('\n\n')
+            blocks = [((block.split(), 100), (rotate(block.split()), 1)) for block in blocks]
+            sol1 = part(blocks, 0)
+            sol2 = part(blocks, 1)
         
 if __name__=='__main__':
     main()
