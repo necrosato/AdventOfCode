de468180e5fdc44bc0624d79b9f8dc6ab632db91
Naookie Sato <necrosato@live.com>
2024-12-02 17:40:42 -0800

    solution helpers

--- template/solution.py
+++ template/solution.py
@@ -1,6 +1,10 @@
 import argparse
 import time
+from collections import deque
 
+'''
+some generic helper functions
+'''
 def timer_func(func):
     def wrap_func(*args, **kwargs):
         t1 = time.time()
@@ -9,6 +13,57 @@ def timer_func(func):
         print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s returned {result}')
         return result
     return wrap_func
+
+def int_grid(lines):
+    return [list(map(int, line.split())) for line in lines]
+ 
+def transpose(grid):
+    return list(map(list, zip(*grid)))
+
+def true_func(val):
+    return True
+
+def get_neighbors(grid, row, col, range_val=1, diagonals=False, include_value=True, value_condition=lambda x:True):
+    """Gets neighboring elements within a given range in a grid."""
+    neighbors = []
+    rows, cols = len(grid), len(grid[0])
+    for i in range(row - range_val, row + range_val + 1):
+        for j in range(col - range_val, col + range_val + 1):
+            if 0 <= i < rows and 0 <= j < cols and (i != row or j != col):
+                if diagonals or i == row or j == col:
+                    if value_condition(grid[i][j]):
+                        neighbors.append((i,j,grid[i][j]) if include_value else (i,j)) 
+    return neighbors
+
+def bfs_grid(grid, row, col):
+    visited = set()
+    order = []
+    queue = deque([(row, col, grid[row][col])])
+    result = []
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.add(node)
+            order.append(node)
+            result.append(node)
+            queue.extend(get_neighbors(grid, node[0], node[1]))
+    return result, order
+
+def dfs_grid(grid, row, col, visited=None, order=None):
+    if visited==None:
+        visited=set()
+    if order==None:
+        order=[]
+    node = (row, col, grid[row][col])
+    visited.add(node)
+    order.append(node)
+    for neighbor in get_neighbors(grid, row, col):
+        if neighbor not in visited:
+            dfs_grid(grid, neighbor[0], neighbor[1], visited, order)
+    return visited, order
+'''
+end generic helper functions
+'''
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Advent of Code Solution', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -29,7 +84,7 @@ def main():
     for input_file_name in args.input:
         with open(input_file_name, 'r') as f:
             lines = [l.strip() for l in f.readlines()]
-            grid = []
+            grid = int_grid(lines)
             sol1 = part1(grid)
             sol2 = part2(grid)
         
