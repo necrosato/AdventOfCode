6bbd913fc0c4585887ad7281de5e4e32660f78c6
Naookie Sato <necrosato@live.com>
2024-12-16 12:10:22 -0800

    faster

--- 2024/day16/solution.py
+++ 2024/day16/solution.py
@@ -32,19 +32,10 @@ def instrument(silent=False):
         return wrap_func
     return decorator
 
-def vector_add(v1, v2):
-    return tuple(x + y for x, y in zip(v1, v2))
-
-def int_grid(lines):
-    return [list(map(int, line.split())) for line in lines]
-
 dirs =[(-1, 0),(0, 1),(1, 0),(0, -1)]
 def coordinate_list(grid):
     return [(i, j, grid[i][j], d) for i in range(len(grid)) for j in range(len(grid[i])) for d in dirs]
  
-def transpose(grid):
-    return list(map(list, zip(*grid)))
-
 def get_neighbors(grid, row, col, d, range_val=1, diagonals=False, include_value=True, value_condition=lambda x:True):
     """Gets neighboring elements within a given range in a grid."""
     neighbors = []
@@ -59,19 +50,24 @@ def get_neighbors(grid, row, col, d, range_val=1, diagonals=False, include_value
                         neighbors.append((i,j,grid[i][j], nd, cost) if include_value else (i,j, nd, cost)) 
     return neighbors
 
+def get_neighbors2(grid, row, col, d, value_condition=lambda x:True):
+    """Gets neighboring elements within a given range in a grid."""
+    neighbors = []
+    rows, cols = len(grid), len(grid[0])
+    i = row-d[0]
+    j = col-d[1]
+    if value_condition(grid[i][j]):
+        for d2 in dirs:
+            cost = 1 if d2 == d else 1001
+            neighbors.append((i,j,grid[i][j], d2, cost)) 
+    return neighbors
+
+
 def grid_find(grid, val):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == val:
                 return (i, j, val)
-
-def grid_find_all(grid, val):
-    vals = []
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == val:
-                vals.append((i, j, val))
-    return vals
 
 def dijkstra_grid(grid, start):
     distances = {node: float('inf') for node in coordinate_list(grid)}
@@ -91,6 +87,25 @@ def dijkstra_grid(grid, start):
                 heapq.heappush(pq, (distance, neighbor[:-1]))
     return distances
 
+def dijkstra_grid2(grid, start):
+    distances = {node: float('inf') for node in coordinate_list(grid)}
+    distances[start] = 0
+    visited = set()
+    pq = [(0, start)]
+    while pq:
+        current_distance, current_node = heapq.heappop(pq)
+        if current_node in visited:
+            continue
+        visited.add(current_node)
+        row, col, val, d = current_node
+        for neighbor in get_neighbors2(grid, row, col, d, value_condition=lambda x:x!='#'):
+            distance = current_distance + neighbor[-1]
+            if distance < distances[neighbor[:-1]]:
+                distances[neighbor[:-1]] = distance
+                heapq.heappush(pq, (distance, neighbor[:-1]))
+    return distances
+
+
 '''
 end generic helper functions
 '''
@@ -104,20 +119,18 @@ def parseArgs():
 @timer_func
 def part1( grid ):
     start = (*grid_find(grid, 'S'), (0, 1))
-    end = grid_find(grid, 'E')
     results = dijkstra_grid(grid, start)
-    ends = []
-    for r in results:
-        if r[0:3] == end:
-            ends.append(results[r])
-    return min(ends) 
-
-def get_ends(results):
-    ends = []
+    ends = {}
     for r in results:
         if r[2] == 'E':
-            ends.append((r, results[r]))
-    return ends
+            ends[r] = results[r]
+    return min(ends.values()) 
+
+def best_result(results_map, ijcd):
+    res = set()
+    for r in results_map:
+        res.add(results_map[r][ijcd])
+    return min(res)
  
 @timer_func
 def part2( grid ):
@@ -130,23 +143,15 @@ def part2( grid ):
     best_len = min(ends.values()) 
     end_results = {}
     for e in ends:
-        if ends[e] == best_len:
-            end_results[e] = dijkstra_grid(grid, e)
-    #print(end_results)
-    #print(results)
+        end_results[e] = dijkstra_grid2(grid, e)
     bps = set()
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             c = grid[i][j]
             for d in dirs:
                 ijcd = (i, j, c, d)
-                results2 = dijkstra_grid(grid, ijcd)
-                print(ijcd)
-                if ijcd in results:
-                    ends2 = get_ends(results2)
-                    for end, endlen in ends2:
-                        if best_len-results[ijcd]-results2[end] == 0:
-                            bps.add((i, j))
+                if best_len == results[ijcd] + best_result(end_results, ijcd):
+                    bps.add((i, j))
     return len(bps)
 
 def main():
