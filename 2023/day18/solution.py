8be3c3bfb4e7270bdee230ab678cf9558931bbe2
Naookie Sato <necrosato@live.com>
2024-12-19 20:39:51 -0800

    part 1

--- 2023/day18/solution.py
+++ 2023/day18/solution.py
@@ -34,15 +34,34 @@ def instrument(silent=False):
 
 def vector_add(v1, v2):
     return tuple(x + y for x, y in zip(v1, v2))
+'''
+end generic helper functions
+'''
 
-def int_grid(lines):
-    return [list(map(int, line.split())) for line in lines]
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Advent of Code Solution', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input', type=str, action='append', required=True,
+        help='an input file path, can passed multiple times to run multiple test files')
+    return parser.parse_args()
 
-def coordinate_list(grid):
-    return [(i, j, grid[i][j]) for i in range(len(grid)) for j in range(len(grid[i]))]
- 
-def transpose(grid):
-    return list(map(list, zip(*grid)))
+def setToGrid(lava):
+    mini = min([l[0][0] for l in lava])
+    maxi = max([l[0][0] for l in lava])
+    minj = min([l[0][1] for l in lava])
+    maxj = max([l[0][1] for l in lava])
+    ioff = 0-mini
+    joff = 0-minj
+    grid = []
+    print(maxi-mini+1, maxj-minj+1)
+    for i in range(maxi-mini+1):
+        grid.append([])
+        print(i)
+        for j in range(maxj-minj+1):
+            grid[-1].append('.')
+    for l in lava:
+        grid[l[0][0]+ioff][l[0][1]+joff]= '#'
+    return grid
+
 
 def get_neighbors(grid, row, col):
     neighbors = []
@@ -51,64 +70,19 @@ def get_neighbors(grid, row, col):
         i = row+pair[0]
         j = col+pair[1]
         if 0 <= i < rows and 0 <= j < cols:
-            neighbors.append(grid[i][j])
+            if grid[i][j] != '#':
+                neighbors.append((i,j,grid[i][j]))
     return neighbors
 
-def get_neighbors2(grid, row, col, range_val=1, diagonals=False, include_value=True, value_condition=lambda x:True):
-    """Gets neighboring elements within a given range in a grid."""
-    neighbors = []
-    rows, cols = len(grid), len(grid[0])
-    for i in range(row - range_val, row + range_val + 1):
-        for j in range(col - range_val, col + range_val + 1):
-            if 0 <= i < rows and 0 <= j < cols and (i != row or j != col):
-                if diagonals or i == row or j == col:
-                    if value_condition(grid[i][j]):
-                        neighbors.append((i,j,grid[i][j]) if include_value else (i,j)) 
-    return neighbors
-
-def bfs_grid(grid, row, col):
-    visited = set()
-    order = []
-    queue = deque([(row, col, grid[row][col])])
-    result = []
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            visited.add(node)
-            order.append(node)
-            result.append(node)
-            queue.extend(get_neighbors(grid, node[0], node[1]))
-    return result, order
-
-def dfs_grid(grid, row, col, visited=None, order=None):
-    if visited==None:
-        visited=set()
-    if order==None:
-        order=[]
-    node = (row, col, grid[row][col])
-    visited.add(node)
-    order.append(node)
-    for neighbor in get_neighbors(grid, row, col):
-        if neighbor not in visited:
-            dfs_grid(grid, neighbor[0], neighbor[1], visited, order)
-    return visited, order
-
-def dijkstra(graph, start):
-    distances = {node: float('inf') for node in graph}
-    distances[start] = 0
-    visited = set()
-    pq = [(0, start)]
-    while pq:
-        current_distance, current_node = heapq.heappop(pq)
-        if current_node in visited:
-            continue
-        visited.add(current_node)
-        for neighbor, weight in graph[current_node].items():
-            distance = current_distance + weight
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(pq, (distance, neighbor))
-    return distances
+def flood(grid, start):
+    perim = grid_find_all(grid, '#')
+    inside = [start]
+    while inside:
+        i, j, v = inside.pop()
+        grid[i][j] = '#'
+        for neighbor in get_neighbors(grid, i, j):
+            inside.append(neighbor)
+    return grid
 
 def grid_find(grid, val):
     for i in range(len(grid)):
@@ -124,51 +98,49 @@ def grid_find_all(grid, val):
                 vals.append((i, j, val))
     return vals
 
-def dijkstra_grid(grid, start_val):
-    start = grid_find(grid, start_val) 
-    distances = {node: float('inf') for node in coordinate_list(grid)}
-    distances[start] = 0
-    visited = set()
-    pq = [(0, start)]
-    while pq:
-        current_distance, current_node = heapq.heappop(pq)
-        if current_node in visited:
-            continue
-        visited.add(current_node)
-        row, col, val = current_node
-        for neighbor in get_neighbors(grid, row, col, value_condition=lambda x:x!='#'):
-            distance = current_distance + 1
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(pq, (distance, neighbor))
-    return distances
-
-'''
-end generic helper functions
-'''
-
-def parseArgs():
-    parser = argparse.ArgumentParser(description='Advent of Code Solution', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-i', '--input', type=str, action='append', required=True,
-        help='an input file path, can passed multiple times to run multiple test files')
-    return parser.parse_args()
+@timer_func
+def part1( moves ):
+    dirs = {'U':(-1,0),'D':(1,0),'R':(0,1),'L':(0,-1)}
+    lava = set()
+    pos = (0, 0)
+    lava.add((pos, None))
+    for direction, dist, color in moves:
+        for i in range(dist):
+            pos=vector_add(pos, dirs[direction])
+            lava.add((pos, color))
+    grid = setToGrid(lava)
+    print('\n'.join([''.join(l) for l in grid]))
+    i,j,v = grid_find(grid, '#')
+    flood(grid, (i+1, j+1, '.'))
+    print()
+    print('\n'.join([''.join(l) for l in grid]))
+    return sum([l.count('#') for l in grid])
 
 @timer_func
-def part1( grid ):
-    pass
-
-@timer_func
-def part2( grid ):
-    pass
+def part2( moves ):
+    dirs = {3:(-1,0),1:(1,0),0:(0,1),2:(0,-1)}
+    lava = set()
+    pos = (0, 0)
+    lava.add((pos, None))
+    for direction, dist, color in moves:
+        direction = int(color[7])
+        dist = int(color[2:6], 16)
+        for i in range(dist):
+            pos=vector_add(pos, dirs[direction])
+            lava.add((pos, color))
+    grid = setToGrid(lava)
+    i,j,v = grid_find(grid, '#')
+    flood(grid, (i+1, j+1, '.'))
+    return sum([l.count('#') for l in grid])
 
 def main():
     args = parseArgs()
     for input_file_name in args.input:
         with open(input_file_name, 'r') as f:
-            lines = [l.strip() for l in f.readlines()]
-            grid = lines
-            sol1 = part1(grid)
-            sol2 = part2(grid)
+            lines = [l.strip().split() for l in f.readlines()]
+            moves = [(l[0], int(l[1]), l[2]) for l in lines]
+            sol1 = part1(moves)
+            sol2 = part2(moves)
             for timing in list(reversed(sorted(timings, key=lambda x:x[1]))):
                 print('function {} took {} seconds total'.format(timing, timings[timing]))
          
