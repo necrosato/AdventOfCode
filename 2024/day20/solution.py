ae17851f6a7bc3acad8781b0dd5d9e3bb55662e5
Naookie Sato <necrosato@live.com>
2024-12-19 23:11:33 -0800

    super slow

---
+++ 2024/day20/solution.py
@@ -0,0 +1,130 @@
+import argparse
+import time
+from collections import deque
+import heapq
+
+'''
+some generic helper functions
+'''
+def timer_func(func):
+    def wrap_func(*args, **kwargs):
+        t1 = time.time()
+        result = func(*args, **kwargs)
+        t2 = time.time()
+        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s returned {result}')
+        return result
+    return wrap_func
+
+timings = {}
+def instrument(silent=False):
+    def decorator(func):
+        def wrap_func(*args, **kwargs):
+            t1 = time.time()
+            result = func(*args, **kwargs)
+            t2 = time.time()
+            duration = t2-t1
+            if not silent:
+                print(f'Function {func.__name__!r} executed in {(duration):.4f}s returned {result}')
+            if func.__name__ not in timings:
+                timings[func.__name__] = 0
+            timings[func.__name__] += duration 
+            return result
+        return wrap_func
+    return decorator
+
+def coordinate_list(grid):
+    return [(i, j, grid[i][j]) for i in range(len(grid)) for j in range(len(grid[i]))]
+ 
+def get_neighbors(grid, row, col, range_val=1, diagonals=False, include_value=True, value_condition=lambda x:True):
+    """Gets neighboring elements within a given range in a grid."""
+    neighbors = []
+    rows, cols = len(grid), len(grid[0])
+    for i in range(row - range_val, row + range_val + 1):
+        for j in range(col - range_val, col + range_val + 1):
+            if 0 <= i < rows and 0 <= j < cols and (i != row or j != col):
+                if diagonals or i == row or j == col:
+                    if value_condition(grid[i][j]):
+                        neighbors.append((i,j,grid[i][j]) if include_value else (i,j)) 
+    return neighbors
+
+def grid_find(grid, val):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == val:
+                return (i, j, val)
+
+def dijkstra_grid(grid, start):
+    distances = {node: float('inf') for node in coordinate_list(grid)}
+    distances[start] = 0
+    visited = set()
+    pq = [(0, start)]
+    while pq:
+        current_distance, current_node = heapq.heappop(pq)
+        if current_node in visited:
+            continue
+        visited.add(current_node)
+        row, col, val = current_node
+        for neighbor in get_neighbors(grid, row, col, value_condition=lambda x:x!='#'):
+            distance = current_distance + 1
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+    return distances
+
+'''
+end generic helper functions
+'''
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description='Advent of Code Solution', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input', type=str, action='append', required=True,
+        help='an input file path, can passed multiple times to run multiple test files')
+    return parser.parse_args()
+
+def vector_sub(v1, v2):
+    return (abs(v2[0]-v1[0]),abs(v2[1]-v1[1]))
+
+def solve(grid, cheatlen, minsave):
+    start = grid_find(grid, 'S') 
+    end = grid_find(grid, 'E') 
+    results = dijkstra_grid(grid, start)
+    from_end = dijkstra_grid(grid, end)
+    cheat_results = {}
+    for node in results:
+        dist = results[node]
+        for node2 in from_end:
+            dist2 = from_end[node2]
+            if dist != float('inf') and dist2 != float('inf'):
+                distance = vector_sub(node2, node)
+                if sum(distance) <= cheatlen:
+                    cheat_results[(node, node2)] = (dist + dist2)+sum(distance)
+    saved = {}
+    for cheat in cheat_results:
+        save = results[end] - cheat_results[cheat]
+        if save >= minsave:
+            if save not in saved:
+                saved[save] = 0
+            saved[save] += 1
+    return sum(saved.values())
+
+@timer_func
+def part1( grid ):
+    return solve(grid, 2, 100)
+
+@timer_func
+def part2( grid ):
+    return solve(grid, 20, 100)
+
+def main():
+    args = parseArgs()
+    for input_file_name in args.input:
+        with open(input_file_name, 'r') as f:
+            grid = [list(l.strip()) for l in f.readlines()]
+            sol1 = part1(grid)
+            sol2 = part2(grid)
+            for timing in list(reversed(sorted(timings, key=lambda x:x[1]))):
+                print('function {} took {} seconds total'.format(timing, timings[timing]))
+         
+if __name__=='__main__':
+    main()
+
