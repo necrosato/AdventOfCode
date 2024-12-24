b488332520066ba850b733912ec1b6cd1b96c4fc
Naookie Sato <necrosato@live.com>
2024-12-24 09:41:10 -0800

    day 24 part 2

--- 2024/day24/solution.py
+++ 2024/day24/solution.py
@@ -2,6 +2,7 @@ import argparse
 import time
 from collections import deque
 import heapq
+from itertools import combinations
 
 '''
 some generic helper functions
@@ -14,135 +15,6 @@ def timer_func(func):
         print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s returned {result}')
         return result
     return wrap_func
-
-timings = {}
-def instrument(silent=False):
-    def decorator(func):
-        def wrap_func(*args, **kwargs):
-            t1 = time.time()
-            result = func(*args, **kwargs)
-            t2 = time.time()
-            duration = t2-t1
-            if not silent:
-                print(f'Function {func.__name__!r} executed in {(duration):.4f}s returned {result}')
-            if func.__name__ not in timings:
-                timings[func.__name__] = 0
-            timings[func.__name__] += duration 
-            return result
-        return wrap_func
-    return decorator
-
-def vector_add(v1, v2):
-    return tuple(x + y for x, y in zip(v1, v2))
-
-def int_grid(lines):
-    return [list(map(int, line.split())) for line in lines]
-
-def coordinate_list(grid):
-    return [(i, j, grid[i][j]) for i in range(len(grid)) for j in range(len(grid[i]))]
- 
-def transpose(grid):
-    return list(map(list, zip(*grid)))
-
-def get_neighbors(grid, row, col):
-    neighbors = []
-    rows, cols = len(grid), len(grid[0])
-    for pair in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        i = row+pair[0]
-        j = col+pair[1]
-        if 0 <= i < rows and 0 <= j < cols:
-            neighbors.append(grid[i][j])
-    return neighbors
-
-def get_neighbors2(grid, row, col, range_val=1, diagonals=False, include_value=True, value_condition=lambda x:True):
-    """Gets neighboring elements within a given range in a grid."""
-    neighbors = []
-    rows, cols = len(grid), len(grid[0])
-    for i in range(row - range_val, row + range_val + 1):
-        for j in range(col - range_val, col + range_val + 1):
-            if 0 <= i < rows and 0 <= j < cols and (i != row or j != col):
-                if diagonals or i == row or j == col:
-                    if value_condition(grid[i][j]):
-                        neighbors.append((i,j,grid[i][j]) if include_value else (i,j)) 
-    return neighbors
-
-def bfs_grid(grid, row, col):
-    visited = set()
-    order = []
-    queue = deque([(row, col, grid[row][col])])
-    result = []
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            visited.add(node)
-            order.append(node)
-            result.append(node)
-            queue.extend(get_neighbors(grid, node[0], node[1]))
-    return result, order
-
-def dfs_grid(grid, row, col, visited=None, order=None):
-    if visited==None:
-        visited=set()
-    if order==None:
-        order=[]
-    node = (row, col, grid[row][col])
-    visited.add(node)
-    order.append(node)
-    for neighbor in get_neighbors(grid, row, col):
-        if neighbor not in visited:
-            dfs_grid(grid, neighbor[0], neighbor[1], visited, order)
-    return visited, order
-
-def dijkstra(graph, start):
-    distances = {node: float('inf') for node in graph}
-    distances[start] = 0
-    visited = set()
-    pq = [(0, start)]
-    while pq:
-        current_distance, current_node = heapq.heappop(pq)
-        if current_node in visited:
-            continue
-        visited.add(current_node)
-        for neighbor, weight in graph[current_node].items():
-            distance = current_distance + weight
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(pq, (distance, neighbor))
-    return distances
-
-def grid_find(grid, val):
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == val:
-                return (i, j, val)
-
-def grid_find_all(grid, val):
-    vals = []
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == val:
-                vals.append((i, j, val))
-    return vals
-
-def dijkstra_grid(grid, start_val):
-    start = grid_find(grid, start_val) 
-    distances = {node: float('inf') for node in coordinate_list(grid)}
-    distances[start] = 0
-    visited = set()
-    pq = [(0, start)]
-    while pq:
-        current_distance, current_node = heapq.heappop(pq)
-        if current_node in visited:
-            continue
-        visited.add(current_node)
-        row, col, val = current_node
-        for neighbor in get_neighbors(grid, row, col, value_condition=lambda x:x!='#'):
-            distance = current_distance + 1
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(pq, (distance, neighbor))
-    return distances
-
 '''
 end generic helper functions
 '''
@@ -153,19 +25,16 @@ def parseArgs():
         help='an input file path, can passed multiple times to run multiple test files')
     return parser.parse_args()
 
-ops = {
-        'AND': lambda x,y: x&y,
-        'OR': lambda x,y: x|y,
-        'XOR': lambda x,y: x^y
-        }
 @timer_func
 def part1( wires, gates ):
+    ops = {
+            'AND': lambda x,y: x&y,
+            'OR': lambda x,y: x|y,
+            'XOR': lambda x,y: x^y
+            }
     all_wires = set()
     for i1,op,i2,ar,dest in gates:
-        all_wires.add(i1)
-        all_wires.add(i2)
-        all_wires.add(dest)
-    done = False
+        all_wires |= set((i1,i2,dest))
     while not all_wires.issubset(set(wires.keys())):
         for i1,op,i2,ar,dest in gates:
             if i1 in wires and i2 in wires:
@@ -176,20 +45,46 @@ def part1( wires, gates ):
             res += str(wires[wire])
     return int(res,2)
 
+# Function to find the corresponding register for an expression
+def find_expression(op1, op, op2, registers):
+    for key, val in registers.items():
+        if val == f'{op1} {op} {op2}' or val == f'{op2} {op} {op1}':
+            return key
+    return None
+
 @timer_func
-def part2( wires, gates ):
-    xres = ''
-    yres = ''
-    zres = ''
-    for wire in reversed(sorted(wires)):
-        if wire[0] == 'x':
-            xres += str(wires[wire])
-        if wire[0] == 'y':
-            yres += str(wires[wire])
-        if wire[0] == 'z':
-            zres += str(wires[wire])
-    print(xres,yres,zres)
-    pass
+def part2( registers ):
+    swaps = []
+    index = 0
+    carry_reg = ''
+    while f'x{index:02d}' in registers and len(swaps) < 8:
+        x_reg = f'x{index:02d}'
+        y_reg = f'y{index:02d}'
+        z_reg = f'z{index:02d}'
+
+        if index == 0:
+            carry_reg = find_expression(x_reg, 'AND', y_reg, registers)
+        else:
+            xor_reg = find_expression(x_reg, 'XOR', y_reg, registers)
+            and_reg = find_expression(x_reg, 'AND', y_reg, registers)
+            carry_in_reg = find_expression(xor_reg, 'XOR', carry_reg, registers)
+
+            if carry_in_reg is None:
+                swaps.extend([xor_reg, and_reg])
+                registers[xor_reg], registers[and_reg] = registers[and_reg], registers[xor_reg]
+                index = 0
+                continue
+
+            if carry_in_reg != z_reg:
+                swaps.extend([carry_in_reg, z_reg])
+                registers[carry_in_reg], registers[z_reg] = registers[z_reg], registers[carry_in_reg]
+                index = 0
+                continue
+
+            carry_in_reg = find_expression(xor_reg, 'AND', carry_reg, registers)
+            carry_reg = find_expression(and_reg, 'OR', carry_in_reg, registers)
+        index += 1
+    return ','.join(sorted(swaps))
 
 def main():
     args = parseArgs()
@@ -199,10 +94,15 @@ def main():
             wires = [l.split(': ') for l in p1.strip().split('\n')]
             wires = {k:int(v) for k,v in wires}
             gates = [l.split() for l in p2.strip().split('\n')]
+            registers = {}
+            for line in p1.splitlines():
+                name, value = line.split(': ')
+                registers[name] = value
+            for line in p2.splitlines():
+                value, name = line.split(' -> ')
+                registers[name] = value
             sol1 = part1(wires, gates)
-            sol2 = part2(wires, gates)
-            for timing in list(reversed(sorted(timings, key=lambda x:x[1]))):
-                print('function {} took {} seconds total'.format(timing, timings[timing]))
+            sol2 = part2(registers)
          
 if __name__=='__main__':
     main()
